0b8ab6a4f89ea79e1e9da884e7f1368d1e12d1bf
largoui <largou.imane.mpsi@gmail.com>
2024-03-23 00:37:24 +0100

    Words to lower case & filter

---
+++ src/main/java/if4030/kafka/LineFilter.java
@@ -0,0 +1,84 @@
+package if4030.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.regex.Pattern;
+
+public final class LineFilter {
+
+    public static final String INPUT_TOPIC = "lines-stream";
+    public static final String OUTPUT_TOPIC = "words-stream";
+
+    static Properties getStreamsConfig(final String[] args) throws IOException {
+        final Properties props = new Properties();
+        if (args != null && args.length > 0) {
+            try (final FileInputStream fis = new FileInputStream(args[0])) {
+                props.load(fis);
+            }
+            if (args.length > 1) {
+                System.out.println("Warning: Some command line arguments were ignored. This demo only accepts an optional configuration file.");
+            }
+        }
+        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "streams-linefilter");
+        props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.putIfAbsent(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 0);
+        props.putIfAbsent(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.putIfAbsent(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // setting offset reset to earliest so that we can re-run the demo code with the same pre-loaded data
+        // Note: To re-run the demo, you need to use the offset reset tool:
+        // https://cwiki.apache.org/confluence/display/KAFKA/Kafka+Streams+Application+Reset+Tool
+        props.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        return props;
+    }
+
+    static void createLineFilterStream(final StreamsBuilder builder) {
+        final KStream<String, String> source = builder.stream(INPUT_TOPIC);
+
+        final KStream<String, String> filterStream = source
+            .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("[\\s\\p{Punct}&&[^-]]+")))
+            .filter((key, value) -> Pattern.matches("[a-zàâçéèêëîïôûùüÿñæœ]+(-[a-zàâçéèêëîïôûùüÿñæœ]+)*", value));
+        
+        filterStream.to(OUTPUT_TOPIC,Produced.with(Serdes.String(), Serdes.String()));
+    }
+
+    public static void main(final String[] args) throws IOException {
+        final Properties props = getStreamsConfig(args);
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        createLineFilterStream(builder);
+        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-linefilter-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (final Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+}
+
