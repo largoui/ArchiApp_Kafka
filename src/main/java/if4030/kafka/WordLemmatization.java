1e2a057813070425b581b56276995d801a5a5c67
largoui <largou.imane.mpsi@gmail.com>
2024-03-23 03:55:56 +0100

    Words Lemmatization

--- src/main/java/if4030/kafka/WordLemmatization.java
+++ src/main/java/if4030/kafka/WordLemmatization.java
@@ -3,29 +3,27 @@ package if4030.kafka;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
-import java.util.regex.Pattern;
 
 public final class WordLemmatization {
 
-    public static final String INPUT_TOPIC = "lines-stream";
+    public static final String INPUT_TOPIC = "words-stream";
     public static final String OUTPUT_TOPIC = "tagged-words-stream";
 
     static Properties getStreamsConfig(final String[] args) throws IOException {
@@ -38,7 +36,7 @@ public final class WordLemmatization {
                 System.out.println("Warning: Some command line arguments were ignored. This demo only accepts an optional configuration file.");
             }
         }
-        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "streams-linefilter");
+        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "streams-lemmatization");
         props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.putIfAbsent(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 0);
         props.putIfAbsent(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
@@ -53,19 +51,45 @@ public final class WordLemmatization {
 
     static void createLineFilterStream(final StreamsBuilder builder) throws IOException {
         final KStream<String, String> source = builder.stream(INPUT_TOPIC);
+        Map<String,List<String>> lexique = Lexique();
+
+        final KStream<String, String> lemmeStream = source
+        .map((key, value) -> {
+            String word;
+            String category;
+            if (lexique.containsKey(value)) {
+                word = lexique.get(value).get(0);
+                category = lexique.get(value).get(1);
+            } else {
+                word = value;
+                category = null;
+            }
+            System.out.println(category +  " " + word);
+            return new KeyValue<String,String>(category, word);
+        });
+        lemmeStream.peek(( key, word ) -> System.out.println( "key: " + key + " - word: " + word ));
+        lemmeStream.to(OUTPUT_TOPIC,Produced.with(Serdes.String(), Serdes.String()));
+    }
+
+    static Map<String,List<String>> Lexique() throws FileNotFoundException {
         Map<String,List<String>> lexique = new HashMap<>();
-        BufferedReader reader = new BufferedReader( new FileReader( "Lexique.csv" ));
-        String line = reader.readLine();
-        while(line != null){
-            String[] wordDesc = line.split(";");
-            lexique.put(wordDesc[0],Arrays.asList(wordDesc[1],wordDesc[2]));
+        String fileName = "Lexique.csv";
+        try (BufferedReader reader = new BufferedReader( new FileReader( fileName ))) {
+            String line;
             line = reader.readLine();
+            while(line != null){
+                String[] wordDesc = line.split(";");
+                if (wordDesc.length>2){
+                    lexique.put(wordDesc[0],Arrays.asList(wordDesc[1],wordDesc[2]));
+                }
+                line = reader.readLine();
+            } 
+        } catch (FileNotFoundException e) {
+            throw e;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        final KStream<String, String> filterStream = source
-            .flatMapValues(value -> Arrays.asList(value.toLowerCase(Locale.getDefault()).split("[\\s\\p{Punct}&&[^-]]+")))
-            .filter((key, value) -> Pattern.matches("[a-zàâçéèêëîïôûùüÿñæœ]+(-[a-zàâçéèêëîïôûùüÿñæœ]+)*", value));
-        
-        filterStream.to(OUTPUT_TOPIC,Produced.with(Serdes.String(), Serdes.String()));
+        return lexique;
     }
 
     public static void main(final String[] args) throws IOException {
@@ -77,7 +101,7 @@ public final class WordLemmatization {
         final CountDownLatch latch = new CountDownLatch(1);
 
         // attach shutdown handler to catch control-c
-        Runtime.getRuntime().addShutdownHook(new Thread("streams-linefilter-shutdown-hook") {
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-lemmatization-shutdown-hook") {
             @Override
             public void run() {
                 streams.close();
